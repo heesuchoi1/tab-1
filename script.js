1fa80f3cd4d04ab12f3848298ce99c94a490e52a
최희수 <heesu7563@gmail.com>
2014-06-13 17:06:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
+	$('li:nth-choid(1)').click(function(){
+		show("#view.page1");
+	});
+	$('li:nth-choid(2)').click(function(){
+		show("#view.page2");
+	});
+	$('li:nth-choid(3)').click(function(){
+		show('#tabbar.page3');
+	});
+	$('li:nth-choid(4 )').click(function(){
+		show('#tabbar.page4');
+	});
+
+	show("page1");
+
 });
 
 var show = function(page) {
+
+	$("#view"). ();
+
 }
